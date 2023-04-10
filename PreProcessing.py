4bf7ec19d0a9e9a6132cde90b66687d775579526
tommy-qiu <tq326@nyu.edu>
2023-04-10 10:29:59 -0400

    Add files via upload
    The Python file is designed to preprocess audio files for a machine learning workflow that requires input data of the same number of channels and the same audio length. The script uses PyTorch libraries and functions to standardize the audio files and prepare them for use in the machine learning model.
    
    Once the audio files have been standardized, the script saves the preprocessed data to a new file format suitable for use in the machine learning workflow. This file format may include additional metadata, such as labels or timestamps, to help with data analysis and model training.
    
    The preprocessing script also includes options for adjusting the audio processing parameters to suit specific use cases. For example, the user may specify a target audio length or number of channels to accommodate different types of audio signals.
    
    Overall, the Python preprocessing script is a powerful tool for preparing audio data for use in machine learning workflows that require standardized input data. By automating the audio signal processing and standardization tasks, the script saves time and ensures consistency in the data, enabling more accurate and effective machine learning models.

---
+++ PreProcessing.py
@@ -0,0 +1,109 @@
+import math, random
+import matplotlib.pyplot as plt
+import torch
+import torchaudio
+from torchaudio import transforms
+from IPython.display import Audio
+
+class AudioUtil():
+  # ----------------------------
+  # Load an audio file. Return the signal as a tensor and the sample rate
+  # ----------------------------
+  @staticmethod
+  def open(audio_file):
+    sig, sr = torchaudio.load(audio_file)
+    return (sig, sr)
+  # ----------------------------
+  # Convert the given audio to the desired number of channels
+  # ----------------------------
+  @staticmethod
+  def rechannel(aud, new_channel):
+    sig, sr = aud
+
+    if (sig.shape[0] == new_channel):
+      # Nothing to do
+      return aud
+
+    if (new_channel == 1):
+      # Convert from stereo to mono by selecting only the first channel
+      resig = sig[:1, :]
+    else:
+      # Convert from mono to stereo by duplicating the first channel
+      resig = torch.cat([sig, sig])
+
+    return ((resig, sr))
+
+  # ----------------------------
+  # Since Resample applies to a single channel, we resample one channel at a time
+  # ----------------------------
+  @staticmethod
+  def resample(aud, newsr):
+    sig, sr = aud
+
+    if (sr == newsr):
+      # Nothing to do
+      return aud
+
+    num_channels = sig.shape[0]
+    # Resample first channel
+    resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
+    if (num_channels > 1):
+      # Resample the second channel and merge both channels
+      retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
+      resig = torch.cat([resig, retwo])
+
+    return ((resig, newsr))
+
+  # ----------------------------
+  # Pad (or truncate) the signal to a fixed length 'max_ms' in milliseconds
+  # ----------------------------
+  @staticmethod
+  def pad_trunc(aud, max_ms):
+    sig, sr = aud
+    num_rows, sig_len = sig.shape
+    max_len = sr//1000 * max_ms
+
+    if (sig_len > max_len):
+      # Truncate the signal to the given length
+      sig = sig[:,:max_len]
+
+    elif (sig_len < max_len):
+      # Length of padding to add at the beginning and end of the signal
+      pad_begin_len = random.randint(0, max_len - sig_len)
+      pad_end_len = max_len - sig_len - pad_begin_len
+
+      # Pad with 0s
+      pad_begin = torch.zeros((num_rows, pad_begin_len))
+      pad_end = torch.zeros((num_rows, pad_end_len))
+
+      sig = torch.cat((pad_begin, sig, pad_end), 1)
+      
+    return (sig, sr)
+
+
+
+def plot_waveform(waveform, sample_rate):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"Channel {c+1}")
+    figure.suptitle("waveform")
+    print("inside")
+    plt.show()
+    # plt.show(block=False)
+
+
+    
+test = AudioUtil()
+z = test.open("test.wav")
+plot_waveform(z[0],z[1])
+resample
